d4c7342fc41b39c9612b740db3e5e2d4cd82068b
Yashas Shah <79524299+Tr1angular@users.noreply.github.com>
2021-06-21 12:26:54 -0400

    js

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ document.querySelector('#app').innerHTML = `
   <h1>Welcome to my first Vite site!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+
+
+
+
+
+
+
+
+
+
+
+// 
